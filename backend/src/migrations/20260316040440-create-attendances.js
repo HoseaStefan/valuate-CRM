618ec3812a9715081ce2ad90285817198a73ad7a
AloysiusCristian <cristianaloysius@gmail.com>
2026-03-16 11:09:54 +0700

    Added attendance table

---
+++ backend/src/migrations/20260316040440-create-attendances.js
@@ -0,0 +1,49 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('attendances', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      date: {
+        allowNull: false,
+        type: Sequelize.DATEONLY
+      },
+      clockIn: {
+        allowNull: false,
+        typ: Sequelize.TIME
+      },
+      clockOut: {
+        allowNull: false,
+        typ: Sequelize.TIME
+      },
+      status: {
+        allowNull: false,
+        type: Sequelize.ENUM('present', 'absent', 'late', 'excused')
+      },
+      notes: {
+        allowNull: true,
+        type: Sequelize.STRING
+      }
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('attendances');
+  }
+};
