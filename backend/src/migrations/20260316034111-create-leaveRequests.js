7003ef2b09eae7f60d7c93daa73831b4e467fca0
AloysiusCristian <cristianaloysius@gmail.com>
2026-03-16 10:48:52 +0700

    Added create leave requests

--- backend/src/migrations/20260316034111-create-leaveRequests.js
+++ backend/src/migrations/20260316034111-create-leaveRequests.js
@@ -3,20 +3,64 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('leaveRequests', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      reason: {
+        allowNull: false,
+        type: Sequelize.ENUM('vacation', 'sick leave', 'personal leave', 'other')
+      },
+      startDate: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      endDate: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      status: {
+        allowNull: false,
+        type: Sequelize.ENUM('pending', 'approved', 'rejected')
+      },
+      reviewedBy: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      },
+      rejectionReason: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('leaveRequests');
   }
 };
